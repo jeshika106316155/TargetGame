eaf7c6f23bc14e4c5a7cecb388a43f879f59d8f3
unknown <106316155@gms.tcu.edu.tw>
2020-05-09 16:33:33 +0800

    apphomecontainer

--- js/app.js
+++ js/app.js
@@ -39,26 +39,16 @@ function init() {
         backgroundimg.y = 0;
         app.stage.addChild(backgroundimg);
 
+        homecontainer = new PIXI.Container();
+        app.stage.addChild(homecontainer);
+
         textup = new PIXI.Sprite.from("images/textup.png");
         textup.anchor.set(0.5);
         textup.height = 150;
         textup.width = 400;
         textup.x = app.view.width / 2;
         textup.y = app.view.height / 4;
-        app.stage.addChild(textup);
-
-        textup2 = new PIXI.Sprite.from("images/textup.png");
-        textup2.position = "absolute";
-        textup2.anchor.set(0.5);
-        textup2.height = 150;
-        textup2.width = 400;
-        textup2.x = 400;
-        textup2.y = 100;
-        textup2.speed = 10;
-        app.ticker.add(delta => gameLoop(delta));
-        app.ticker.add(delta => checkLoop(delta));
-        app.stage.addChild(textup2);
-
+        homecontainer.addChild(textup);
 
         buttonfemale = new PIXI.Sprite.from(texturefe);
         buttonfemale.buttonMode = true;
@@ -72,7 +62,7 @@ function init() {
         buttonfemale.interactive = true;
         buttonfemale.buttonMode = true;
         buttonfemale.on('pointerdown', function (e) { onbuttonfedown(e); }).on("pointerover", function (e) { onbuttonover(e); }).on("pointerout", onbuttonout);
-        app.stage.addChild(buttonfemale);
+        homecontainer.addChild(buttonfemale);
 
         buttonmale = new PIXI.Sprite.from(texturema);
         buttonmale.buttonMode = true;
@@ -85,7 +75,7 @@ function init() {
         buttonmale.interactive = true;
         buttonmale.buttonMode = true;
         buttonmale.on('pointerdown', function (e) { onbuttonfedown(e); }).on("pointerover", function (e) { onbuttonover(e); }).on("pointerout", onbuttonout);
-        app.stage.addChild(buttonmale);
+        homecontainer.addChild(buttonmale);
 
         buttonquestion = new PIXI.Sprite.from(this.textureque);
         buttonquestion.buttonMode = true;
@@ -99,12 +89,11 @@ function init() {
         buttonquestion.interactive = true;
         buttonquestion.buttonMode = true;
         buttonquestion.on('pointerdown', onbuttonquestiondown).on("pointerover", function (e) { onbuttonover(e); }).on("pointerout", onbuttonout);
-        app.stage.addChild(buttonquestion);
+        homecontainer.addChild(buttonquestion);
+
     }
-    //var textureButton = PIXI.Texture.fromImage('https://dl.dropboxusercontent.com/s/mi2cibdajml8qj9/arrow_wait.png?dl=0');
 }
 function onbuttonfedown(bt) {
-    //console.log(bt);
     this.isdown = true;
     if (bt.target.myCustomProperty == feimage) {
         bt.target._texture = texturefe2;
@@ -143,7 +132,7 @@ function onbuttonout() {
         this.texture = textureque;
     }
 }
-function a() {
+function onbuttonquestiondown() {
     let buttonok;
 
     var h1 = app.view.height / 3;
@@ -162,7 +151,7 @@ function a() {
     buttonok.buttonMode = true;
     buttonok.on('pointerdown', onbuttonokdown);
 
-    var buttonEndTurn = new PIXI.Graphics(); s
+    var buttonEndTurn = new PIXI.Graphics();
     buttonEndTurn.beginFill(0xFF2342);
     buttonEndTurn.drawRect(w1, h1, w2, h1);
     buttonEndTurn.endFill();
@@ -181,24 +170,6 @@ function a() {
 
     app.stage.addChild(buttons);
 }
-function onbuttonquestiondown() {
-    a();
-}
-function onremovebuttonfema() {
-    app.stage.removeChild(buttonfemale);
-    app.stage.removeChild(buttonmale);
-}
 function onbuttonokdown() {
     app.stage.removeChild(buttons);
-    app.stage.addChild(buttonfemale);
-    app.stage.addChild(buttonmale);
-}
-function gameLoop(delta) {
-    textup2.x -= 1;
-    textup2.y += 1;
-}
-function checkLoop(delta) {
-    if (textup2.y == app.view.height / 2) {
-        app.ticker.stop();
-    }
 }
