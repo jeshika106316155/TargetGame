d1cc3c2babca74ffe21875a05ef7ddc9100b060a
Thalia <thalia_noga@yahoo.com>
2020-05-12 12:30:10 +0800

    ganti png female

--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,6 @@
 class CStar extends PIXI.Sprite{
     constructor(container, x) {
-        super(PIXI.Texture.fromImage("Assets/star_empty.png"));
+        super(PIXI.Texture.from("Assets/star_empty.png"));
         this.anchor.set(0);
         this.x = x;
         this.y = 22;
@@ -28,7 +28,7 @@ class CScore {
         this.scoreline.anchor.set(0);
         this.scoreline.x=19;
         this.scoreline.y=26;
-        this.scoreline.width=378;
+        this.scoreline.width=1;
         this.scoreline.height=33;
         this.container.addChild(this.scoreline);
        
@@ -44,36 +44,66 @@ class CScore {
 
 class CBoard extends PIXI.Sprite{
     static count;
-    constructor(pict, x, y, width, height, score, scorebox) {
-        super(PIXI.Texture.fromImage("Assets/" + pict + ".png"));
-        this.pict = pict;
-        this.anchor.set(0);
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    // constructor(pict, x, y, width, height, score, scorebox) {
+    //     super(PIXI.Texture.from("Assets/" + pict + ".png"));
+    //     this.pict = pict;
+    //     this.anchor.set(0);
+    //     this.x = x;
+    //     this.y = y;
+    //     this.width = width;
+    //     this.height = height;
+    //     this.score = score;
+    //     this.scorebox = scorebox;
+    //     femaleContainer.addChild(this);
+    // }
+    constructor(app, sw, sh, color, radius, score, scorebox) {
+        super(PIXI.Texture.from("images/" + color + ".png"));
+        this.app = app;
+        this.sw = sw;
+        this.sh = sh;
+        this.color = color;
+        this.radius = radius;
         this.score = score;
         this.scorebox = scorebox;
+        //this.board = new PIXI.Sprite.from("images/" + color + ".png");
+        this.anchor.set(0.5);
+        this.x = sw / 2;
+        this.y = sh * 1 / 4;
+        this.width = radius * 2;
+        this.height = radius * 2;
         femaleContainer.addChild(this);
     }
 }
 
 CBoard.count = 0;
+
+class CArrow extends PIXI.Sprite{
+    constructor(container, color) {
+        super(PIXI.Texture.from("Assets/"+color+"_dart.png"));
+        this.anchor.set(0);
+        this.color = color;
+        this.x = 188;
+        this.y = 437;
+        this.width = 31;
+        this.height = 97;
+        container.addChild(this);
+    }
+}
+
+
 let gaugeCircle;
 
 function initFemale() {
     sw = app.view.width, sh = app.view.height;
 
-    let yellow, pink, red, uterus;
-    let scorebox;
-    let semicircle, circle;
-
     femaleContainer = new PIXI.Container();
     app.stage.addChild(femaleContainer);
 
-    scorebox = new CScore(femaleContainer, sw, sh);
+    let semicircle, circle;
 
-    dartBorder = new PIXI.Sprite.from("Assets/border.png");
+    let scorebox = new CScore(femaleContainer, sw, sh);
+
+    let dartBorder = new PIXI.Sprite.from("Assets/border.png");
     dartBorder.anchor.set(0);
     dartBorder.x = 64;
     dartBorder.y = 90;
@@ -81,13 +111,32 @@ function initFemale() {
     dartBorder.height = 290;
     femaleContainer.addChild(dartBorder);
 
-    top_10pt = new CBoard(femaleContainer,"10pt_top", 247, 123,  95, 113, 10, scorebox);
-    bot_10pt = new CBoard(femaleContainer, "10pt_bot",  57, 236, 102, 111, 10, scorebox);
-    top_20pt = new CBoard(femaleContainer, "20pt_top", 67, 125, 98, 108, 20, scorebox);
-    bot_20pt = new CBoard(femaleContainer, "20pt_bot", 252, 237, 93, 110, 20, scorebox);
-    top_30pt = new CBoard(femaleContainer, "30pt_top",132, 98,  129, 77, 30, scorebox);
-    bot_30pt = new CBoard(femaleContainer,"30pt_bot", 141, 292,  134, 84, 30, scorebox);
-    pt40 = new CBoard(femaleContainer,"40pt",  137, 170, 131, 133, 40, scorebox);
+    // top_10pt = new CBoard(femaleContainer,"10pt_top", 247, 123,  95, 113, 10, scorebox);
+    // bot_10pt = new CBoard(femaleContainer, "10pt_bot",  57, 236, 102, 111, 10, scorebox);
+    // top_20pt = new CBoard(femaleContainer, "20pt_top", 67, 125, 98, 108, 20, scorebox);
+    // bot_20pt = new CBoard(femaleContainer, "20pt_bot", 252, 237, 93, 110, 20, scorebox);
+    // top_30pt = new CBoard(femaleContainer, "30pt_top",132, 98,  129, 77, 30, scorebox);
+    // bot_30pt = new CBoard(femaleContainer,"30pt_bot", 141, 292,  134, 84, 30, scorebox);
+    // pt40 = new CBoard(femaleContainer,"40pt",  137, 170, 131, 133, 40, scorebox);
+    let yellow = new CBoard(femaleContainer, sw, sh, "yellow", 100, 10, scorebox);
+    let pink = new CBoard(femaleContainer, sw, sh, "pink", 80, 20, scorebox);
+    let red = new CBoard(femaleContainer, sw, sh, "red", 60, 30, scorebox);
+
+    let uterus = new PIXI.Sprite.from("Assets/uterus.png");
+    uterus.anchor.set(0);
+    uterus.x = 160;
+    uterus.y = 199;
+    uterus.width = 88;
+    uterus.height = 72;
+    femaleContainer.addChild(uterus);
+
+    let wings = new PIXI.Sprite.from("Assets/wings.png");
+    wings.anchor.set(0);
+    wings.x = 1;
+    wings.y = 109;
+    wings.width = 415;
+    wings.height = 342;
+    femaleContainer.addChild(wings);
 
     let txt = new PIXI.Text("", { fontFamily: 'Arial', fontSize: 24, fill: 0x000000, align: 'center' });
     femaleContainer.addChild(txt);
@@ -101,44 +150,50 @@ function initFemale() {
     txt.y = 255;
     femaleContainer.addChild(txt);
 
-    uterus = new PIXI.Sprite.from("Assets/uterus.png");
-    uterus.anchor.set(0);
-    uterus.x = 160;
-    uterus.y = 199;
-    uterus.height = 72;
-    uterus.width = 88;
-    femaleContainer.addChild(uterus);
+    str_bar_empty=new PIXI.Texture.from("Assets/str_bar_empty.png");
+    str_bar_full=new PIXI.Texture.from("Assets/str_bar_full.png");
+    let str_bar = new PIXI.Sprite.from("Assets/str_bar_empty.png");
+    str_bar.anchor.set(0);
+    str_bar.x = 32;
+    str_bar.y = 511;
+    str_bar.width = 381;
+    str_bar.height = 143;
+    femaleContainer.addChild(str_bar);
 
-    var semicircGraphic = new this.PIXI.Graphics();
-    semicircGraphic.beginFill(0x00FF00);
-    semicircGraphic.lineStyle(2, 0x00FF00);
-    semicircGraphic.arc(0, 0, 100, Math.PI, 0); // cx, cy, radius, startAngle, endAngle
-    var semicircTexture = app.renderer.generateTexture(semicircGraphic);
-    semicircle= new PIXI.Sprite(semicircTexture);
-    semicircle.anchor.set(0.5,1);
-    semicircle.x=sw/2;
-    semicircle.y=sh*7/8;
-    femaleContainer.addChild(semicircle);
+    let indicator = new PIXI.Sprite.from("Assets/indicator.png");
+    indicator.anchor.set(0,1);
+    indicator.x = 186.38;
+    indicator.y = 618.84;
+    indicator.width = 20;
+    indicator.height = 48;
+    indicator.angle = 29;
+    femaleContainer.addChild(indicator);
 
-    var circleGraphic = new this.PIXI.Graphics();
-    circleGraphic.beginFill(0xFF0000);
-    circleGraphic.lineStyle(2, 0xFF0000);
-    circleGraphic.drawCircle(0, 0, 20);
-    var circleTexture = app.renderer.generateTexture(circleGraphic);
-    circle= new PIXI.Sprite(circleTexture);
-    circle.anchor.set(0.5);
-    circle.x=sw/2;
-    circle.y=sh*13/16;
-    femaleContainer.addChild(circle);
 
-    arrow = new PIXI.Sprite.from("images/arrow.png");
-    arrow.anchor.set(1, 0.5);
-    arrow.width = 150;
-    arrow.height = 40;
-    arrow.angle = 90;
-    arrow.x = sw / 2;
-    arrow.y = sh * 13 / 16;
-    femaleContainer.addChild(arrow);
+
+    // var semicircGraphic = new this.PIXI.Graphics();
+    // semicircGraphic.beginFill(0x00FF00);
+    // semicircGraphic.lineStyle(2, 0x00FF00);
+    // semicircGraphic.arc(0, 0, 100, Math.PI, 0); // cx, cy, radius, startAngle, endAngle
+    // var semicircTexture = app.renderer.generateTexture(semicircGraphic);
+    // semicircle= new PIXI.Sprite(semicircTexture);
+    // semicircle.anchor.set(0.5,1);
+    // semicircle.x=sw/2;
+    // semicircle.y=sh*7/8;
+    // femaleContainer.addChild(semicircle);
+
+    // var circleGraphic = new this.PIXI.Graphics();
+    // circleGraphic.beginFill(0xFF0000);
+    // circleGraphic.lineStyle(2, 0xFF0000);
+    // circleGraphic.drawCircle(0, 0, 20);
+    // var circleTexture = app.renderer.generateTexture(circleGraphic);
+    // circle= new PIXI.Sprite(circleTexture);
+    // circle.anchor.set(0.5);
+    // circle.x=sw/2;
+    // circle.y=sh*13/16;
+    // femaleContainer.addChild(circle);
+
+    arrow = new CArrow(femaleContainer, "blue")
 
     var k = 1;
     let rotateTicker = () => {
@@ -173,8 +228,11 @@ function initFemale() {
             var centerx = yellow.x, centery = yellow.y;
             var txtx = arrow.x, txty = arrow.y;
             for (var i = 0; i <= arrow.width; i++) {
-                txtx -= Math.cos(arrow.rotation);
-                txty -= Math.sin(arrow.rotation);
+                // txtx -= Math.cos(arrow.rotation);
+                // txty -= Math.sin(arrow.rotation);
+                txty -= Math.cos(arrow.rotation);
+                txtx -= Math.sin(arrow.rotation);
+
             }
 
             var r2 = ((txtx - centerx) * (txtx - centerx)) + ((txty - centery)) * ((txty - centery));
@@ -197,8 +255,10 @@ function initFemale() {
 
         }
 
-        arrow.x = arrow.x - Math.cos(arrow.rotation) * speed;
-        arrow.y = arrow.y - Math.sin(arrow.rotation) * speed;
+        // arrow.x = arrow.x - Math.cos(arrow.rotation) * speed;
+        // arrow.y = arrow.y - Math.sin(arrow.rotation) * speed;
+        arrow.y = arrow.y - Math.cos(arrow.rotation) * speed;
+        arrow.x = arrow.x - Math.sin(arrow.rotation) * speed;
         counter++;
         gaugeCircle.interactive = false;
 
@@ -285,16 +345,16 @@ function hitboard(scorebox, score, txt) {
         counter++;
     }
 
-    if (width < sw * 7 / 8) {
+    if (width < scrbx.scorebg.width) {
         scrbx.scoreline.width += score;
         scrbx.score+=score;
-        if (width > sw * 7 / 8) {
-            width = sw * 7 / 8;
+        if (width >scrbx.scorebg.width) {
+            width = scrbx.scorebg.width;
         }
         txt.text= "SCORE="+scrbx.score;
         offsetX=scrbx.scoreline.x+scrbx.scoreline.width;
         if (offsetX >= scrbx.star1.x && CBoard.count == 0) {
-            scorebg.count++;
+            CBoard.count++;
             scrbx.star1.texture = fullstar;
             app.ticker.add(waitTicker);
             return true;
@@ -305,9 +365,21 @@ function hitboard(scorebox, score, txt) {
             app.ticker.add(waitTicker);
             return true;
         }
-    } else if (CBoard.count == 2) {
+        if (offsetX >= scrbx.star3.x && CBoard.count == 2) {
+            CBoard.count++;
+            scrbx.star3.texture = fullstar;
+            app.ticker.add(waitTicker);
+            return true;
+        }
+        if (offsetX >= scrbx.star4.x && CBoard.count == 3) {
+            CBoard.count++;
+            scrbx.star4.texture = fullstar;
+            app.ticker.add(waitTicker);
+            return true;
+        }
+    } else if (CBoard.count == 4) {
         CBoard.count++;
-        scrbx.star3.texture = fullstar;
+        scrbx.star5.texture = fullstar;
         app.ticker.add(waitTicker);
         return true;
     }
@@ -315,9 +387,7 @@ function hitboard(scorebox, score, txt) {
 }
 
 function initArrow() {
-    arrow.x = sw / 2;
-    arrow.y = sh * 13 / 16;
-    femaleContainer.addChild(arrow);
+    arrow = new CArrow(femaleContainer, "blue");
     counter = 0;
     gaugeCircle.x = sw / 2;
     gaugeCircle.interactive = true;
@@ -325,75 +395,105 @@ function initArrow() {
 
 function displayBox() {
     let buttonok;
-    var textureok = PIXI.Texture.from('images/ok.png');
+    var textureok = PIXI.Texture.from('Assets/understood.png');
 
-    var h1 = app.view.height / 3;
-    var w1 = 30,
-        w2 = app.view.width - (2 * w1);
     buttons = new PIXI.Container();
-    buttons.height = app.view.height;
-    buttons.width = app.view.width;
+    app.stage.addChild(buttons);
 
     buttonok = new PIXI.Sprite.from(textureok);
-    buttonok.buttonMode = true;
-    buttonok.anchor.set(1);
-    buttonok.height = 50;
-    buttonok.width = 50;
-    buttonok.x = w1 + w2;
-    buttonok.y = 2 * h1;
-    buttonok.myCustomProperty = this.okimage;
+    buttonok.anchor.set(0);
+    buttonok.width = 110;
+    buttonok.height = 57;
     buttonok.interactive = true;
-    buttonok.buttonMode = true;
     buttonok.on('pointerdown', onGameOk);
+    buttons.addChild(buttonok)
 
-    buttonReplay = new PIXI.Sprite.from("images/replay.png");
-    buttonReplay.anchor.set(0, 1);
-    buttonReplay.height = 50;
-    buttonReplay.width = 50;
-    buttonReplay.x = w1;
-    buttonReplay.y = 2 * h1;
-    buttonReplay.interactive = true;
-    buttonReplay.on('pointerdown', onReplay);
+    var background = new PIXI.Sprite.from('Assets/girl_star1.png');
+    buttons.addChild(background);
+    switch (CBoard.count){
+        case 1:
+            background = new PIXI.Sprite.from('Assets/girl_star1.png');
+            buttonok.x = 17;
+            buttonok.y = 404;
+            break;
+        case 2:
+            background.texture = PIXI.Texture.from('Assets/girl_star2.png');
+            buttonok.x = 292;
+            buttonok.y = 419;
+            break;
+        case 3:
+            background.texture = PIXI.Texture.from('Assets/girl_star3.png');
+            buttonok.x = 300;
+            buttonok.y = 351;
+            break;
+        case 4:
+            background.texture = PIXI.Texture.from('Assets/girl_star4.png');
+            buttonok.x = 22;
+            buttonok.y = 437;
+            break;
+        case 5:
+            background.texture = PIXI.Texture.from('Assets/girl_star5.png');
+            buttonok.x = 283;
+            buttonok.y = 591;
+            break;
+        case 6:
+            background.texture = PIXI.Texture.from('Assets/end_page.png');
+            buttonok.texture = PIXI.Texture.from('Assets/back_intro.png');
+            buttonok.x = 52;
+            buttonok.y = 562;
+            buttonok.width = 143;
+            buttonok.height = 82;
+            buttonok.on('pointerdown', onEnd);
+            buttonEnd = new PIXI.Sprite.from('Assets/end_game.png');
+            buttonEnd.anchor.set(0);
+            buttonEnd.x = 217;
+            buttonEnd.y = 561;
+            buttonEnd.width = 148;
+            buttonEnd.height = 76;
+            buttonEnd.interactive = true;
+            buttonEnd.on('pointerdown', onEnd);
+            buttons.addChild(buttonEnd);
+            break;
 
-    var buttonEndTurn = new PIXI.Graphics();
-    buttonEndTurn.beginFill(0xFF2342);
-    buttonEndTurn.drawRect(w1, h1, w2, h1);
-    buttonEndTurn.endFill();
-
-    var text = new PIXI.Text("Instruction:");
-    text.font = "50px Arial";
-    text.fill = "0XFFFFFF";
-    text.anchor.set(0.5);
-    text.x = (w1 + w2) / 2;
-    text.y = h1 + text.height;
-
-    var blurbg = new PIXI.Graphics();
-    blurbg.beginFill(0xFFFFFF);
-    blurbg.drawRect(0, 0, app.view.width, app.view.height);
-
-    buttons.addChild(blurbg);
-    buttons.addChild(buttonEndTurn);
-    buttons.addChild(text);
-    if (CBoard.count < 3) { buttons.addChild(buttonok); }
-    else {
-
-        buttons.addChild(buttonReplay);
-        buttonok.texture = new PIXI.Texture.from("images/logout.png");
-        buttons.addChild(buttonok);
     }
-    app.stage.addChild(buttons);
+
+    // buttonReplay = new PIXI.Sprite.from("images/replay.png");
+    // buttonReplay.anchor.set(0, 1);
+    // buttonReplay.height = 50;
+    // buttonReplay.width = 50;
+    // buttonReplay.x = w1;
+    // buttonReplay.y = 2 * h1;
+    // buttonReplay.interactive = true;
+    // buttonReplay.on('pointerdown', onReplay);
+
 }
 
 function onGameOk() {
-    if (CBoard.count < 3) {
-        app.stage.removeChild(buttons);
-        displayed = false;
-        initArrow();
-    }
-    else { onReplay(); };
+        let waitTicker = () => {
+            if (counter == 30){
+                app.ticker.remove(waitTicker);
+                displayBox();
+            }
+            counter++;
+        }
+        if(CBoard.count==5){
+            CBoard.count++;
+            app.ticker.add(waitTicker);
+        }
+        else {
+            app.stage.removeChild(buttons);
+            displayed = false;
+            initArrow();
+        }
 }
 
 function onReplay() {
+    app.stage.removeChild(buttons);
+    app.stage.removeChild(femaleContainer);
+    app.stage.addChild(homecontainer);
+}
+
+function onEnd() {
     app.stage.removeChild(buttons);
     app.stage.removeChild(femaleContainer);
     app.stage.addChild(homecontainer);
